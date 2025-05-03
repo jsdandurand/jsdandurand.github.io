6937ed2f385052258b123c38dc03f411c5a19183
JS Dandurand <jsdandurand@hotmail.com>
2025-05-03 12:31:17 -0600

    updated publication info

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -16,19 +16,19 @@ const Publications = () => {
     {
       title: "Retri3D: 3D Neural Graphics Representation Retrieval",
       authors: "Yushi Guan, Daniel Kwan, Jean Sebastien Dandurand, Xi Yan, Ruofan Liang, Yuxuan Zhang, Nilesh Jain, Nilesh Ahuja, Selvakumar Panneer, Nandita Vijaykumar",
-      conference: "ICLR",
+      conference: "ICLR (Spotlight)",
       year: "2025",
-      abstract: "Accepted at ICLR 2025 (Spotlight)",
+      abstract: "A novel framework that enables accurate and efficient text-based retrieval of 3D scenes represented as Learnable 3D Neural Graphics Representations (3DNGRs)",
       link: "https://gavinguan95.github.io/Retri3D/",
       pdf: "https://openreview.net/pdf?id=q3EbOXb4y1"
     },
     {
-      title: "Physics Informed Machine Learning for Emission Measurement",
-      authors: "Henry Jin, Jean Sebastien Dandurand",
-      conference: "ASME DTOG2024",
-      year: "2024",
-      abstract: "Accepted at Digital Horizons: Energizing Transformation in Oil, Gas and Beyond (ASME DTOG2024): Digital Solutions for Gas Emissions",
-      link: "https://dtog.secure-platform.com/a/solicitations/233/sessiongallery/17665/application/148619"
+      title: "Physics Informed Machine Learning for Multiphase Emission Measurement",
+      authors: "Henry Q Jin, Jean Sebastien Dandurand, Willow Liu",
+      conference: "SPE Gas & Oil Technology Conference",
+      year: "2025",
+      abstract: "Evaluating Physics-Informed Neural Networks (PINNs) as a computationally efficient and accurate alternative to traditional CFD methods for solving multiphase emission dispersion problems, demonstrating their potential for real-time applications and reduced resource demands",
+      link: "https://onepetro.org/SPEGOTS/proceedings-abstract/25GOTS/25GOTS/652797"
     }
   ];
 
